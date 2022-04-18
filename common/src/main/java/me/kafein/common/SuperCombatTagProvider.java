aaa669a0c8c6008457cc3eac9dc19fc25155a4f4
KafeinTR <kafein40@gmail.com>
2022-04-18 20:51:07 +0300

    Create SuperCombatTagProvider

---
+++ common/src/main/java/me/kafein/common/SuperCombatTagProvider.java
@@ -0,0 +1,11 @@
+package me.kafein.common;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class SuperCombatTagProvider {
+
+    @Getter @Setter
+    private static SuperCombatTag superCombatTag;
+
+}
