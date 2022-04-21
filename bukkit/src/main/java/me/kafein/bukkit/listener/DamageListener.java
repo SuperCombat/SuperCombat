613d2c1986340817b09cac8af02817aaedf9e1c5
KafeinTR <kafein40@gmail.com>
2022-04-22 02:18:52 +0300

    Moved some codes

--- bukkit/src/main/java/me/kafein/bukkit/listener/DamageListener.java
+++ bukkit/src/main/java/me/kafein/bukkit/listener/DamageListener.java
@@ -4,10 +4,13 @@ import me.kafein.common.SuperCombatTag;
 import me.kafein.common.config.ConfigKeys;
 import me.kafein.common.tag.Tag;
 import me.kafein.common.tag.TagManager;
+import me.kafein.common.tag.TagReason;
+import me.kafein.common.tag.TagType;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
@@ -17,14 +20,13 @@ public class DamageListener implements Listener {
 
     private final TagManager tagManager = SuperCombatTag.getInstance().getTagManager();
 
-    @EventHandler
+    @EventHandler (priority = EventPriority.LOWEST, ignoreCancelled = true)
     public void onDamage(EntityDamageByEntityEvent e) {
 
         Entity entity = e.getEntity();
         Entity damager = e.getDamager();
 
-        if (!(entity instanceof Player) && !(damager instanceof Player)) return;
-        if (!(entity instanceof Player) && !ConfigKeys.MOB_TAGGING.getValue()) return;
+        if (!(entity instanceof Player) && (!(damager instanceof Player) || !ConfigKeys.MOB_TAGGING.getValue())) return;
 
         tagManager.getTagMap().values().forEach(tag -> Bukkit.broadcastMessage(tag.getUserName() + " : " + tag.getDuration()));
 
@@ -34,6 +36,8 @@ public class DamageListener implements Listener {
             Tag tag = optionalTag.orElseGet(() -> new Tag(player.getName(), player.getUniqueId()));
             tag.setDuration(ConfigKeys.TAG_DURATION.getValue());
             tag.setOtherUser(damager.getName(), damager.getUniqueId());
+            tag.setTagType(TagType.PLAYER);
+            tag.setTagReason(TagReason.DEFENDER);
             tagManager.addTag(tag);
         }
 
@@ -43,6 +47,8 @@ public class DamageListener implements Listener {
             Tag tag = optionalTag.orElseGet(() -> new Tag(player.getName(), player.getUniqueId()));
             tag.setDuration(ConfigKeys.TAG_DURATION.getValue());
             tag.setOtherUser(damager.getName(), damager.getUniqueId());
+            tag.setTagType((entity instanceof Player) ? TagType.PLAYER : TagType.MOB);
+            tag.setTagReason(TagReason.ATTACKER);
             tagManager.addTag(tag);
         }
 
