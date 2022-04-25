9d98b7f9401d1949459f24ffee5bfaf2f70cae4a
KafeinTR <kafein40@gmail.com>
2022-04-25 22:56:42 +0300

    Refactored

--- bukkit/src/main/java/me/kafein/bukkit/listener/BukkitDamageListener.java
+++ bukkit/src/main/java/me/kafein/bukkit/listener/BukkitDamageListener.java
@@ -7,16 +7,19 @@ import me.kafein.common.tag.TagReason;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
 
 public class BukkitDamageListener implements Listener {
 
     private final TagManager tagManager = SuperCombat.getInstance().getTagManager();
 
-    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onDamage(EntityDamageByEntityEvent e) {
 
         Entity defenderEntity = e.getEntity();
@@ -35,6 +38,32 @@ public class BukkitDamageListener implements Listener {
             Player defender = (Player) defenderEntity;
             tagManager.tagPlayer(BukkitTagController.controlPlayer(defender, attacker, TagReason.DEFENDER));
         }
+
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onHit(ProjectileHitEvent event) {
+
+        Projectile projectile = event.getEntity();
+        if (!(projectile.getShooter() instanceof Entity)) return;
+
+        Entity defenderEntity = event.getHitEntity();
+        Entity attackerEntity = (Entity) projectile.getShooter();
+
+        if (!BukkitTagController.isPlayer(attackerEntity) || BukkitTagController.isNPC(attackerEntity)) return;
+
+        tagManager.getTagMap().forEach((player, tag) -> {
+            Bukkit.broadcastMessage(tag.getUserName() + " : " + tag.getDuration());
+        });
+
+        Player attacker = (Player) attackerEntity;
+        tagManager.tagPlayer(BukkitTagController.controlPlayer(attacker, defenderEntity, TagReason.ATTACKER));
+
+        if (defenderEntity instanceof Player) {
+            Player defender = (Player) defenderEntity;
+            tagManager.tagPlayer(BukkitTagController.controlPlayer(defender, attacker, TagReason.DEFENDER));
+        }
+
 
     }
 
--- bukkit/src/main/java/me/kafein/bukkit/listener/BukkitDeathListener.java
+++ bukkit/src/main/java/me/kafein/bukkit/listener/BukkitDeathListener.java
@@ -2,26 +2,52 @@ package me.kafein.bukkit.listener;
 
 import me.kafein.bukkit.tag.BukkitTagController;
 import me.kafein.common.SuperCombat;
+import me.kafein.common.config.ConfigKeys;
+import me.kafein.common.tag.Tag;
 import me.kafein.common.tag.TagManager;
+import me.kafein.common.tag.untag.UntagReason;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public class BukkitDeathListener implements Listener {
 
     private final TagManager tagManager = SuperCombat.getInstance().getTagManager();
 
-    @EventHandler (priority = EventPriority.LOWEST, ignoreCancelled = true)
+    @EventHandler (priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onDeath(PlayerDeathEvent event) {
+
+        Player player = event.getEntity();
+        if (player.hasMetadata("NPC")) return;
+        UUID uuid = player.getUniqueId();
+
+        Optional<Tag> optionalTag = tagManager.getTag(uuid);
+        if (!optionalTag.isPresent()) return;
+        Tag tag = optionalTag.get();
+
+        if (ConfigKeys.Settings.DEATH_UNTAGGING_ENEMY.getValue()) tagManager.unTagPlayer(tag.getOtherUserUUID(), UntagReason.ENEMY_DEATH);
+        if (ConfigKeys.Settings.DEATH_UNTAGGING_SELF.getValue()) tagManager.unTagPlayer(uuid, UntagReason.SELF_DEATH);
+
+    }
+
+    @EventHandler (priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onDeath(EntityDeathEvent event) {
 
         Entity entity = event.getEntity();
-        if (!BukkitTagController.isPlayer(entity) || BukkitTagController.isNPC(entity)) return;
+        UUID uuid = entity.getUniqueId();
 
-        Player player = (Player) entity;
-        tagManager.unTagPlayer(player.getUniqueId());
+        Optional<Tag> optionalTag = tagManager.getTag(uuid);
+        if (!optionalTag.isPresent()) return;
+        Tag tag = optionalTag.get();
+
+        if (ConfigKeys.Settings.DEATH_UNTAGGING_ENEMY.getValue()) tagManager.unTagPlayer(tag.getOtherUserUUID(), UntagReason.ENEMY_DEATH);
 
     }
 
