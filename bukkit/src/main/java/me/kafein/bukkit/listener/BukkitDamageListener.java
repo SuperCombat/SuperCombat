1c539c3451c0c7dbc7693233bff709bc881bf887
KafeinTR <kafein40@gmail.com>
2022-04-25 16:43:29 +0300

    Refactored

--- bukkit/src/main/java/me/kafein/bukkit/listener/BukkitDamageListener.java
+++ bukkit/src/main/java/me/kafein/bukkit/listener/BukkitDamageListener.java
@@ -1,11 +1,9 @@
 package me.kafein.bukkit.listener;
 
-import me.kafein.common.SuperCombatTag;
-import me.kafein.common.config.ConfigKeys;
-import me.kafein.common.tag.Tag;
+import me.kafein.bukkit.tag.BukkitTagController;
+import me.kafein.common.SuperCombat;
 import me.kafein.common.tag.TagManager;
 import me.kafein.common.tag.TagReason;
-import me.kafein.common.tag.TagType;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -14,42 +12,28 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
-import java.util.Optional;
-
 public class BukkitDamageListener implements Listener {
 
-    private final TagManager tagManager = SuperCombatTag.getInstance().getTagManager();
+    private final TagManager tagManager = SuperCombat.getInstance().getTagManager();
 
-    @EventHandler (priority = EventPriority.LOWEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
     public void onDamage(EntityDamageByEntityEvent e) {
 
-        Entity entity = e.getEntity();
-        Entity damager = e.getDamager();
+        Entity defenderEntity = e.getEntity();
+        Entity attackerEntity = e.getDamager();
 
-        if (!(entity instanceof Player) && (!(damager instanceof Player) || !ConfigKeys.MOB_TAGGING.getValue())) return;
+        if (!BukkitTagController.isPlayer(attackerEntity) || BukkitTagController.isNPC(attackerEntity)) return;
 
-        tagManager.getTagMap().values().forEach(tag -> Bukkit.broadcastMessage(tag.getUserName() + " : " + tag.getDuration()));
+        tagManager.getTagMap().forEach((player, tag) -> {
+            Bukkit.broadcastMessage(tag.getUserName() + " : " + tag.getDuration());
+        });
 
-        if (entity instanceof Player) {
-            Player player = (Player) entity;
-            Optional<Tag> optionalTag = tagManager.getTag(player.getUniqueId());
-            Tag tag = optionalTag.orElseGet(() -> new Tag(player.getName(), player.getUniqueId()));
-            tag.setDuration(ConfigKeys.TAG_DURATION.getValue());
-            tag.setOtherUser(damager.getName(), damager.getUniqueId());
-            tag.setTagType(TagType.PLAYER);
-            tag.setTagReason(TagReason.DEFENDER);
-            tagManager.addTag(tag);
-        }
+        Player attacker = (Player) attackerEntity;
+        tagManager.tagPlayer(BukkitTagController.controlPlayer(attacker, defenderEntity, TagReason.ATTACKER));
 
-        if (damager instanceof Player) {
-            Player player = (Player) damager;
-            Optional<Tag> optionalTag = tagManager.getTag(player.getUniqueId());
-            Tag tag = optionalTag.orElseGet(() -> new Tag(player.getName(), player.getUniqueId()));
-            tag.setDuration(ConfigKeys.TAG_DURATION.getValue());
-            tag.setOtherUser(damager.getName(), damager.getUniqueId());
-            tag.setTagType((entity instanceof Player) ? TagType.PLAYER : TagType.MOB);
-            tag.setTagReason(TagReason.ATTACKER);
-            tagManager.addTag(tag);
+        if (defenderEntity instanceof Player) {
+            Player defender = (Player) defenderEntity;
+            tagManager.tagPlayer(BukkitTagController.controlPlayer(defender, attacker, TagReason.DEFENDER));
         }
 
     }
