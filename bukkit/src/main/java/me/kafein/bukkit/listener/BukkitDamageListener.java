e64130d2319d2c3c6dc99bac20dea9cb4032b525
KafeinTR <kafein40@gmail.com>
2022-05-02 22:23:04 +0300

    Refactored

--- bukkit/src/main/java/me/kafein/bukkit/listener/BukkitDamageListener.java
+++ bukkit/src/main/java/me/kafein/bukkit/listener/BukkitDamageListener.java
@@ -2,16 +2,12 @@ package me.kafein.bukkit.listener;
 
 import me.kafein.bukkit.tag.BukkitTagController;
 import me.kafein.common.SuperCombat;
-import me.kafein.common.tag.TagManager;
 import me.kafein.common.tag.TagReason;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityShootBowEvent;
-import org.bukkit.event.entity.ProjectileHitEvent;
 
 public class BukkitDamageListener implements Listener {
 
@@ -33,10 +29,6 @@ public class BukkitDamageListener implements Listener {
 
         if (!BukkitTagController.isPlayer(attackerEntity)) return;
 
-        plugin.getTagManager().getTagMap().forEach((player, tag) -> {
-            Bukkit.broadcastMessage(tag.getUserName() + " : " + tag.getDuration());
-        });
-
         Player attacker = (Player) attackerEntity;
         plugin.getTagManager().tagPlayer(BukkitTagController.controlPlayer(attacker, defenderEntity, TagReason.ATTACKER));
 
@@ -46,29 +38,5 @@ public class BukkitDamageListener implements Listener {
         }
 
     }
-
-    /*@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onHit(ProjectileHitEvent event) {
-
-        Entity defenderEntity =
-        Bukkit.broadcastMessage(defenderEntity.getType().name());
-        Entity attackerEntity = (Entity) event.getEntity().getShooter();
-
-        if (!(defenderEntity instanceof LivingEntity)) return;
-        if (!BukkitTagController.isPlayer(attackerEntity) || BukkitTagController.isNPC(attackerEntity)) return;
-
-        plugin.getTagManager(.getTagMap().forEach((player, tag) -> {
-            Bukkit.broadcastMessage(tag.getUserName() + " : " + tag.getDuration());
-        });
-
-        Player attacker = (Player) attackerEntity;
-        plugin.getTagManager(.tagPlayer(BukkitTagController.controlPlayer(attacker, defenderEntity, TagReason.ATTACKER));
-
-        if (defenderEntity instanceof Player) {
-            Player defender = (Player) defenderEntity;
-            plugin.getTagManager(.tagPlayer(BukkitTagController.controlPlayer(defender, attacker, TagReason.DEFENDER));
-        }
-
-    }*/
 
 }
