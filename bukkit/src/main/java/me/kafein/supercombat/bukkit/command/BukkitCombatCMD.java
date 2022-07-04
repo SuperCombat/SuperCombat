79f7c791de49d6fcd8738542ed49da9b486ac417
KafeinTR <kafein40@gmail.com>
2022-07-04 17:16:37 +0300

    Improvements for BukkitCombatCMD

--- bukkit/src/main/java/me/kafein/supercombat/bukkit/command/BukkitCombatCMD.java
+++ bukkit/src/main/java/me/kafein/supercombat/bukkit/command/BukkitCombatCMD.java
@@ -1,54 +1,78 @@
 package me.kafein.supercombat.bukkit.command;
 
 import co.aikar.commands.BaseCommand;
-import co.aikar.commands.annotation.CommandAlias;
-import co.aikar.commands.annotation.Description;
-import co.aikar.commands.annotation.HelpCommand;
-import co.aikar.commands.annotation.Subcommand;
-import me.kafein.supercombat.bukkit.SuperCombatPlugin;
-import me.kafein.supercombat.bukkit.util.ColorSerializer;
+import co.aikar.commands.annotation.*;
+import me.kafein.supercombat.bukkit.util.language.LanguageUtil;
+import me.kafein.supercombat.common.blocker.Blockers;
+import me.kafein.supercombat.common.config.handler.ConfigHandler;
 import me.kafein.supercombat.common.config.key.ConfigKeys;
 import me.kafein.supercombat.common.expansion.handler.ExpansionHandler;
 import me.kafein.supercombat.common.tag.handler.TagHandler;
+import me.kafein.supercombat.common.tag.untag.UntagReason;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
-@CommandAlias("supercombattag|supercombat|combattag|sct|sc|ct")
+@CommandAlias("supercombat")
 @Description("SuperCombat main command.")
 @SuppressWarnings("unused")
 public class BukkitCombatCMD extends BaseCommand {
 
-    private final SuperCombatPlugin plugin = SuperCombatPlugin.getInstance();
-
     @HelpCommand
     @Description("Shows the help message.")
-    public void onHelp(CommandSender sender) {
-        ConfigKeys.Language.HELP_MESSAGE.getValue().forEach(message -> {
-            sender.sendMessage(ColorSerializer.serialize(message));
-        });
+    public void helpCommand(CommandSender sender) {
+        if (!hasPermission(sender)) return;
+        LanguageUtil.replaceList(ConfigKeys.Language.HELP_MESSAGE.getValue())
+                .forEach(sender::sendMessage);
     }
 
     @Subcommand("reload")
     @Description("Reloads the plugin's configs.")
-    public void onReload(CommandSender sender) {
-        if (!sender.hasPermission(ConfigKeys.Settings.ADMIN_PERM.getValue())) {
-            sender.sendMessage(ColorSerializer.serialize(ConfigKeys.Language.NO_PERMISSION.getValue()));
-            return;
-        }
-        plugin.loadConfigs();
-        sender.sendMessage("§aCombatTag config reloaded.");
+    public void reloadCommand(CommandSender sender) {
+        if (!hasPermission(sender)) return;
+        ConfigHandler.init();
+        Blockers.init();
         ExpansionHandler.reloadConfigAll();
+        sender.sendMessage(LanguageUtil.replace(ConfigKeys.Language.RELOADED.getValue()));
     }
 
-    @Subcommand("list")
-    @Description("Shows the list of tagged players.")
-    public void onList(CommandSender sender) {
-        if (!sender.hasPermission(ConfigKeys.Settings.ADMIN_PERM.getValue())) {
-            sender.sendMessage(ColorSerializer.serialize(ConfigKeys.Language.NO_PERMISSION.getValue()));
+    @Subcommand("untag")
+    @CommandCompletion("@players")
+    @Description("Untag the player.")
+    public void untagCommand(CommandSender sender, String[] args) {
+        if (!hasPermission(sender)) return;
+        if (args.length < 1) {
+            sender.sendMessage("§6/supercombat untag <player>");
             return;
         }
-        TagHandler.getTagMap().values().forEach(tag -> {
-            sender.sendMessage(tag.getUser().getName() + " : " + tag.getDuration());
-        });
+        String playerName = args[0];
+        Player player = Bukkit.getPlayer(playerName);
+        if (player == null) {
+            sender.sendMessage(LanguageUtil.replace(ConfigKeys.Language.PLAYER_NOT_FOUND.getValue(),
+                    new String[]{"%player%"},
+                    new String[]{playerName}
+            ));
+            return;
+        }
+        TagHandler.unTagPlayer(player.getUniqueId(), UntagReason.UNKNOWN);
+        player.sendMessage("§6" + player.getName() + " &eis untagged.");
+    }
+
+    private boolean hasPermission(CommandSender sender) {
+        if (!sender.hasPermission(ConfigKeys.Settings.ADMIN_PERM.getValue())) {
+            Plugin plugin = Bukkit.getPluginManager().getPlugin("SuperCombat");
+            sender.sendMessage("");
+            sender.sendMessage("  §6§l❘ §e§lSuperCombat");
+            sender.sendMessage("");
+            sender.sendMessage("  §e§l▪ §7Author§8: §eKafein");
+            sender.sendMessage("  §e§l▪ §7Version§8: §e" + plugin.getDescription().getVersion());
+            sender.sendMessage("  §e§l▪ §7Discord§8: §ediscord.gg/vx95fKG7fw");
+            sender.sendMessage("");
+            sender.sendMessage(LanguageUtil.replace(ConfigKeys.Language.NO_PERMISSION.getValue()));
+            return false;
+        }
+        return true;
     }
 
 }
